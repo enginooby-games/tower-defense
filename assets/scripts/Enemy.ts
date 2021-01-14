bb589c4a64d4edd07f259432c79e02fc564b218a
Hieu Ngo <justacpulover@gmail.com>
2021-01-14 12:37:40 +0100

    Implement enemy movement

--- assets/scripts/Enemy.ts
+++ assets/scripts/Enemy.ts
@@ -13,17 +13,45 @@ type Target = {
 export default class NewClass extends cc.Component {
     @property(LevelMap)
     levelMap: LevelMap = null
+    @property
+    speed: number = 150
 
     targets: Target[] = []
-    currentTargetName: number
+    currentTargetName: number = 0
 
     // onLoad () {}
 
     start() {
         this.targets = this.levelMap.pathGroup.getObjects()
 
-        this.currentTargetName = 2
-        this.node.setPosition(this.getCurrentTargetPos())
+        this.moveToNextTarget()
+    }
+
+    moveToNextTarget() {
+        this.currentTargetName++
+        const nextPos: cc.Vec2 = this.getCurrentTargetPos()
+
+        if (!nextPos) {
+            this.node.emit('finish')
+            this.node.destroy()
+        } else {
+            this.moveTo(nextPos).then(() => {
+                this.moveToNextTarget()
+            })
+        }
+    }
+
+    moveTo(nexPos: cc.Vec2): Promise<number> {
+        const xDistance: number = nexPos.x - this.node.x
+        const yDistance: number = nexPos.y - this.node.y
+        const distance: number = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2))
+
+        const time: number = distance / this.speed
+        return new Promise<number>((resolve, reject) => {
+            const moveToAction: cc.ActionInterval = cc.moveTo(time, nexPos)
+            const sequence: cc.ActionInterval = cc.sequence(moveToAction, cc.callFunc(resolve))
+            this.node.runAction(sequence)
+        })
     }
 
     getCurrentTarget(): Target {
@@ -42,5 +70,6 @@ export default class NewClass extends cc.Component {
         return cc.v2(pos.x + this.levelMap.tileSize.width / 2, pos.y + this.levelMap.tileSize.height / 2)
     }
 
-    // update (dt) {}
+    update(dt) {
+    }
 }
